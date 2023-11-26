2951fff1bfcc766d8bf26f1b7c566bb3fb0b1b27
Lars Hoeg <lars.hoeg@verivox.com>
2023-11-26 01:49:49 +0100

    use swr and route handler

---
+++ src/app/api/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getData } from "../utils/getData"
+
+export async function GET({ nextUrl }: NextRequest) {
+    const salary = nextUrl.searchParams.get('salary') || undefined
+    const data = await getData(salary)
+
+    return Response.json(data)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
+'use client'
+
 import { CountryTable } from './components/CountryTable';
-import { useSearchParams } from 'next/navigation';
-import { getData } from './utils/getData';
+import { Data } from './utils/getData';
+import useSWR from 'swr';
 
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
-export default async function Home() {
-  const data = await getData()
-  const germanyData = data.countries.find(entry => entry.country === 'Germany')
+export default function Home({ searchParams }: { searchParams: { salary?: string } }) {
+	const { salary } = searchParams
+	const { data } = useSWR<Data>(`/api${salary ? '?salary=' + salary : ''}`, fetcher)
+	const germanyData = data?.countries.find((entry) => entry.country === 'Germany')
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header>
-        COL index and wages for {data.salaryBeforeTax} euro
-      </header>
-      <div>
-        <p>Money after tax: {germanyData?.netPay}</p>
-        <p>Money after expenses: {(Number(germanyData?.netPay.replaceAll(/\D/g, '') ?? 0)) - 12 * 1500}</p>
-        <p>Money after all: {(Number(germanyData?.netPay.replaceAll(/\D/g, '') ?? 0)) - 12 * 2500}</p>
-      </div>
-      <CountryTable countries={data.countries} />
-    </main>
-  )
+	if (!data) return null;
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<header>
+				COL index and wages for {data?.salaryBeforeTax} euro
+			</header>
+			<div>
+				<p>Money after tax: {germanyData?.netPay}</p>
+				<p>Money after expenses: {(Number(germanyData?.netPay.replace(/\D+/g, '') ?? 0)) - 12 * 1500}</p>
+				<p>Money after all: {(Number(germanyData?.netPay.replace(/\D+/g, '') ?? 0)) - 12 * 2500}</p>
+			</div>
+			<CountryTable countries={data.countries} />
+		</main>
+	)
 }
